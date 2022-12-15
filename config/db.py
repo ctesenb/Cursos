e8c8c814d8bc514d20c4fcaf46863394893d9cf2
Hyre <74812707+ctesenb@users.noreply.github.com>
2022-12-15 08:43:54 -0500

    DB Up

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://admin:YSIEAVvV@mysql-97755-0.cloudclusters.net:19995"
+DATABASE_URL = "mysql+mysqlconnector://admin:CJLz0wsg@mysql-100492-0.cloudclusters.net:10123"
 engine = create_engine(DATABASE_URL+'/Cursos')
 meta = MetaData()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
